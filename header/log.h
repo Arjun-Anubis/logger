00a905b97aa4373e188cf60f1abc5bee0ec26634
Arjun-Anubis <anubiz512z@gmail.com>
2022-02-20 21:47:14 +0530

    Added color

--- header/log.h
+++ header/log.h
@@ -1,29 +1,52 @@
-#include <iostream>
+#ifndef LOGH
+#define LOGH
+
 #include <stdio.h>
+#include <time.h>
+
+#define TIME_LEN 12 
 
 #define LOG_PANIC 0
-#define LOG_CRIT  1
+#define LOG_ERR  1
 #define LOG_WARN  2
 #define LOG_MSG   3 
 #define LOG_MTC   4 // Maintainence
 #define LOG_DEBUG 5
 #define LOG_SL    6 // Super Low
+#define LOG_MAX   LOG_SL
 
-struct logger
-{
-	logger( int loglevel );
-	int loglevel;
-	void setlevel( int loglevel );
-	int std( int loglevel, char * fmt, ... );
-	int stdc( char * prefix, char * fmt, ... );
-	int panic( char * fmt, ... );
-	int crit( char * fmt, ... );
-	int warn( char * fmt, ... );
-	int msg( char * fmt, ... );
-	int mtc( char * fmt, ... );
-	int debug( char * fmt, ... );
-	int sl( char * fmt, ... );
-private:
-	int fd;
-};
+#define LOG_S_PANIC  "Panic"
+#define LOG_S_ERR  "Error"
+#define LOG_S_WARN  "Warn"
+#define LOG_S_MSG  "Message"
+#define LOG_S_MTC  "Mtc"
+#define LOG_S_DEBUG  "Debug"
+#define LOG_S_SL  "SL"
+
+extern int LOGLEVEL;
+extern int LOG_NEWLINE;
+extern int log_fd;
+
+void log_setlevel( int loglevel );
+
+int log_std( const char * file, int line, int loglevel, const char * fmt, ...);
+int log_stdc( const char * prefix, const char * fmt, ... );
+
+int log_test(void);
+
+#define log_panic(...) ( log_std( __FILE__, __LINE__, LOG_PANIC, __VA_ARGS__ ) )
+#define log_err(...) ( log_std( __FILE__, __LINE__, LOG_ERR, __VA_ARGS__ ) )
+#define log_warn(...) ( log_std( __FILE__, __LINE__, LOG_WARN, __VA_ARGS__ ) )
+#define log_msg(...) ( log_std( __FILE__, __LINE__, LOG_MSG, __VA_ARGS__ ) )
+#define log_mtc(...) ( log_std( __FILE__, __LINE__, LOG_MTC, __VA_ARGS__ ) )
+#define log_debug(...) ( log_std( __FILE__, __LINE__, LOG_DEBUG, __VA_ARGS__ ) )
+#define log_sl(...) ( log_std( __FILE__, __LINE__, LOG_SL, __VA_ARGS__ ) )
+
+/*
+ * Aliases
+ */
+
+#define log_info(...) ( log_std( __FILE__, __LINE__, LOG_MSG, __VA_ARGS__ ) )
+
+#endif // LOGH
 
--- src/log.cc
+++ src/log.cc
@@ -1,28 +1,125 @@
+#include <cstdlib>
 #include <stdarg.h>
 
+#include "colors.h"
 #include "log.h"
 
-logger::logger( int loglevel )
-	:loglevel(loglevel)
-{}
+/* logger::logger( int loglevel ) */
+/* 	:loglevel(loglevel),newline(true) */
+/* {} */
+int LOGLEVEL = LOG_MAX;
+int LOG_NEWLINE = 1;
 
-void logger::setlevel( int loglevel )
+void log_setlevel( int loglevel )
 {
-	this->loglevel = loglevel;
+	LOGLEVEL = loglevel;
 	return;
 }
 
-int logger::stdc( char * prefix, char * format, ...)
+int vlog_stdc( const char * prefix, const char * format, va_list args )
+{
+	int result;
+
+	/*
+	 * Needed for it to find where the arguments went, so format is the last named argument
+	 */
+
+	printf( "%s", prefix );
+	result = vprintf( format, args );
+
+	if (LOG_NEWLINE)
+		printf( "\n" );
+
+	return result;
+}
+
+int log_stdc( const char * prefix, const char * format, ...)
 {
 	int result;
 	va_list args;
 	va_start( args, format ); 
+
 	/*
 	 * Needed for it to find where the arguments went, so format is the last named argument
 	 */
-	printf( "Printed by logger\n" );
 
+	printf( "%s", prefix );
 	result = vprintf( format, args );
 
+	if (LOG_NEWLINE)
+		printf( "\n" );
+
 	return result;
+}
+
+int log_std( const char * file, int line_number, int loglevel, const char * format, ... )
+{
+	char * prefix;
+	int return_code;
+	va_list args;
+	va_start( args, format );
+
+	char time_s[TIME_LEN];
+	time_t now = time(0);
+	struct tm  tstruct;
+
+	tstruct = *localtime(&now);
+
+	strftime( time_s, TIME_LEN, "%H:%M:%S", &tstruct );
+
+	char prefix_fmt[] = COLOR_GREEN "%s" COLOR_RESET " " COLOR_BOLD COLOR_YELLOW "%s" COLOR_RESET ":" COLOR_CYAN "%d" COLOR_RESET "\t%s\t";
+
+	switch (loglevel)
+	{
+		case LOG_PANIC:
+			if ( LOGLEVEL >= LOG_PANIC )
+				asprintf( &prefix, prefix_fmt, time_s, file, line_number, COLOR_RED COLOR_BLINK LOG_S_PANIC COLOR_RESET );
+			else return -1;
+			break;
+		case LOG_ERR:
+			if ( LOGLEVEL >= LOG_ERR )
+				asprintf( &prefix, prefix_fmt, time_s, file, line_number, COLOR_RED LOG_S_ERR COLOR_RESET );
+			else return -1;
+			break;
+		case LOG_WARN:
+			if ( LOGLEVEL >= LOG_WARN )
+				asprintf( &prefix, prefix_fmt, time_s, file, line_number, COLOR_YELLOW LOG_S_WARN COLOR_RESET );
+			else return -1;
+			break;
+		case LOG_MSG:
+			if ( LOGLEVEL >= LOG_MSG )
+				asprintf( &prefix, prefix_fmt, time_s, file, line_number, COLOR_GREEN LOG_S_MSG COLOR_RESET );
+			else return -1;
+			break;
+		case LOG_MTC:
+			if ( LOGLEVEL >= LOG_MTC )
+				asprintf( &prefix, prefix_fmt, time_s, file, line_number, COLOR_PURPLE LOG_S_MTC COLOR_RESET );
+			else return -1;
+			break;
+		case LOG_DEBUG:
+			if ( LOGLEVEL >= LOG_DEBUG )
+				asprintf( &prefix, prefix_fmt, time_s, file, line_number, COLOR_CYAN LOG_S_DEBUG COLOR_RESET );
+			else return -1;
+			break;
+		case LOG_SL:
+			if ( LOGLEVEL >= LOG_SL )
+				asprintf( &prefix, prefix_fmt, time_s, file, line_number, COLOR_BLUE LOG_S_SL COLOR_RESET );
+			else return -1;
+			break;
+	}
+	return_code = vlog_stdc( prefix, format, args );
+	free(prefix);
+	return return_code;
+}
+
+int log_test( void )
+{
+	log_panic( "Panic" );
+	log_err( "Error" );
+	log_warn( "Warning" );
+	log_msg( "Message" );
+	log_mtc( "mtc" );
+	log_debug( "Debug" );
+	log_sl( "sl" );
+	return 0;
 }
